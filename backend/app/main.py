e0268a199efd8d8cd605b46c92a700010eee9b59
Ty-Wattana <tywattana@gmail.com>
2024-11-13 13:59:10 +0700

    simple chat api

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,25 @@
+# cd backend
+# uvicorn app.main:app --reload
+
 import logging
 
 from fastapi import FastAPI
 
 from app.routers import minio
 
+# LLM ssection
+import os
+from langchain_openai import AzureChatOpenAI
+from langchain_core.messages import HumanMessage, SystemMessage, AIMessage
+from langchain_core.output_parsers import StrOutputParser
+
+model = AzureChatOpenAI(
+    azure_endpoint=os.environ["AZURE_OPENAI_ENDPOINT"],
+    azure_deployment=os.environ["AZURE_OPENAI_DEPLOYMENT_NAME"],
+    openai_api_version=os.environ["AZURE_OPENAI_API_VERSION"],
+)
+
+# API section
 ROOT_PATH = "/api"
 DOCS_PATH = "/docs"
 
@@ -13,12 +29,18 @@ app = FastAPI(
     docs_url=DOCS_PATH,
 )
 
-logger = logging.getLogger(__name__)
+@app.post("/chat", tags=["LLM"])
+async def chat(user_essage:str):
 
+    messages = [
+    SystemMessage(content="You are a helpful assistant."),
+    HumanMessage(content=user_essage),
+]
 
-@app.get("/ping")
-async def ping():
-    logger.info("[main] ping")
-    return {"message": "OK"}
+    result = model.invoke(messages)
+    parser = StrOutputParser()
+    parser_output = parser.invoke(result)
+
+    return {"message": parser_output}
 
 app.include_router(minio.router, prefix="/minio", tags=["minio"])
